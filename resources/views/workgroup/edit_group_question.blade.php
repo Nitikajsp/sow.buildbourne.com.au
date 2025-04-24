d3d08112805587e1fa5ba911c10d2ebbbafaf408
miteshdalsaniya774 <miteshdalsaniya@jspinfotech.com>
2025-04-24 15:18:59 +0530

    Add New field

--- resources/views/workgroup/edit_group_question.blade.php
+++ resources/views/workgroup/edit_group_question.blade.php
@@ -2,112 +2,134 @@
 
 @push('css')
 <link rel="stylesheet" href="{{ asset('css/custom.css') }}" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-touchspin/4.3.1/jquery.bootstrap-touchspin.min.css">
 @endpush
 
 @section('content')
 <div id="app" class="layout-wrapper">
     @include('include.sidebar')
     <div class="main-content">
-
-        <div class="container-fluid">
+        <div class="container-fluid addcartwidth">
             @include('include.navbar')
 
             <div class="row">
-                <div class="col-md-12 d-flex justify-content-between align-items-center page-header">
-                    <a href="{{ route('workgroup.showgroupquestion') }}" class="float-left d-flex text-black">
-                        <i
-                            class="ti ti-arrow-narrow-left border border-dark rounded-circle mx-1 me-2 text-black"></i>Back
+                <div class="col-md-12 d-flex justify-content-between align-items-center py-4">
+                    <a href="{{ route('workgroup.showworkgroup') }}" class="float-left d-flex text-black">
+                        <i class="ti ti-arrow-narrow-left border border-dark rounded-circle mx-1 me-2 text-black"></i>Back
                     </a>
-                    <a href="{{ route('workgroup.showgroupquestion', $workgroupquestion->id) }}"
-                        class="btn btn-primary btn-dark float-end rounded">
-                        View
-                    </a>
-
                 </div>
             </div>
 
-            <!-- <div class="container-fluid"> -->
             <div class="row">
                 <div class="col-md-7">
                     <div class="inner-container ">
-                        <div class="row">
-                            <div class="col-lg-12 ">
-                                <div class="pull-left">
-                                    <h2>Edit Group Question</h2>
-                                </div>
-                                <div class="pull-left">
-                                    <h5>Please update the details below</h5>
-                                </div>
-                            </div>
-                        </div>
+                        <h2>Edit Work Group Question</h2>
 
-                        @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        @endif
-
-                        @if(session('success'))
+                        @if (session('success'))
                         <div class="alert alert-success">
                             {{ session('success') }}
                         </div>
                         @endif
 
-                        <form action="{{ route('workgroup.workgroupquestionupdate', $workgroupquestion->id) }}"
-                            method="POST" id="editPartyForm">
-                            @csrf
-                            @method('PUT')
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-12 col-md-12 mb-3">
-                                    <div class="form-group">
-                                        <p class="text-secondary mb-1">Group Name</p>
-                                        <select name="workgroup_id" class="form-control border border-white-50">
-                                            <option value="">-- Select Group --</option>
-                                            @foreach($groups as $group)
-                                            <option value="{{ $group->id }}"
-                                                {{ $workgroupquestion->workgroup_id == $group->id ? 'selected' : '' }}>
-                                                {{ $group->group_name }}
-                                            </option>
-                                            @endforeach
-                                        </select>
-                                        <div class="invalid-feedback"></div>
-                                    </div>
-                                </div>
-                                <div class="col-xs-12 col-sm-12 col-md-12 mb-3">
-                                    <div class="form-group">
-                                        <p class="text-secondary mb-1">Question Title</p>
-                                        <input type="text" name="question_title"
-                                            value="{{ old('question_title', $workgroupquestion->question_title) }}"
-                                            class="form-control border border-white-50" placeholder="Question Title">
-                                        <div class="invalid-feedback"></div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <p class="text-secondary mb-1">Question Value</p>
-                                    <input type="text" name="question_value"
-                                        value="{{ old('question_value', $workgroupquestion->question_value) }}"
-                                        class="form-control border border-white-50" placeholder="Question Value">
-                                    <div class="invalid-feedback"></div>
-                                </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                @csrf
+                                <div id="build-wrap"></div>
+                                <button id="update-form" type="submit" class="mt-4 px-4 py-2 bg-blue-600 text-white">Update Form</button>
+                                <button type="button" id="addSubfield" class="mt-4 px-4 py-2 bg-green-600 text-white">Add Subfield</button>
                             </div>
-                            <div class="pull-right mt-3 text-center">
-                                <button type="submit" class="btn btn-primary btn btn-dark me-1 rounded">Save</button>
-                                <a class="btn btn-outline-dark waves-effect rounded"
-                                    href="{{ route('workgroup.showgroupquestion') }}">Cancel</a>
-                            </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
-            <!-- </div> -->
+            <div class="fb-render">
+
+            </div>
         </div>
     </div>
-    @endsection
+</div>
 
-    @push('scripts')
-    @endpush
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"></script>
+<script src="https://formbuilder.online/assets/js/form-builder.min.js"></script>
+<script src="https://formbuilder.online/assets/js/form-render.min.js"></script>
+
+<script>
+    $(function() {
+        const questionId = @json($questionId);
+        const savedJson = @json($questionJson);
+        var existingFormData = @json($questionJson ?? []);
+
+        var fb = $('#build-wrap').formBuilder({
+            formData: existingFormData
+        });
+
+        let subfieldCounter = existingFormData.length + 1;
+
+        $('#addSubfield').on('click', function() {
+            const uniqueSuffix = subfieldCounter++;
+
+            fb.actions.addField({
+                type: 'radio-group',
+                label: `Subfield Options ${uniqueSuffix}`,
+                name: `subfield_options_${uniqueSuffix}`,
+                values: [{
+                    label: 'Option 1',
+                    value: 'option1'
+                }, {
+                    label: 'Option 2',
+                    value: 'option2'
+                }],
+                className: 'form-control'
+            });
+
+            fb.actions.addField({
+                type: 'text',
+                label: `Subfield Question ${uniqueSuffix}`,
+                name: `subfield_question_${uniqueSuffix}`,
+                className: 'form-control'
+            });
+
+            fb.actions.addField({
+                type: 'email',
+                label: `Subfield Email ${uniqueSuffix}`,
+                name: `subfield_email_${uniqueSuffix}`,
+                className: 'form-control'
+            });
+
+            fb.actions.addField({
+                type: 'text',
+                label: `Subfield Address ${uniqueSuffix}`,
+                name: `subfield_address_${uniqueSuffix}`,
+                className: 'form-control'
+            });
+        });
+
+        $('#update-form').on('click', function() {
+
+            const formJson = fb.actions.getData('json');
+
+            $.ajax({
+                url: "{{ route('workgroup.workgroupquestionupdate', ['id' => $questionId]) }}",
+                method: 'POST',
+                data: {
+                    form_data: formJson,
+
+                    _token: '{{ csrf_token() }}'
+                },
+                success: function(res) {
+                    alert('Form updated successfully!');
+                },
+                error: function(err) {
+                    alert('Error updating form.');
+                    console.error(err);
+                }
+            });
+        });
+
+        $('.fb-render').formRender({
+            formData: existingFormData
+        });
+    });
+</script>
+@endsection
